c2345c1c67c585392d98d6a4e827c263dfe1a741
Pawan kumar <kumarpy5890@gmail.com>
2026-05-04 21:39:36 +0530

    Update file_creation.py

--- file_creation.py
+++ file_creation.py
@@ -1,7 +1,7 @@
 #Step 1st: create and open file
 
 # Creating a file and writing initial student data
-
+'''
 file=open("students_details.txt","w")
 
 file.write("101,Adil, 90\n")
@@ -9,3 +9,31 @@ file.write("102,Pawan, 99\n")
 file.write("103,Rahul, 70\n")
 
 file.close()
+'''
+'''
+#Step 2: Search student
+
+search_id="102"
+found=False
+file=open("students_details.txt","r")
+for line in file:
+    data=line.strip().split(",")
+    if data[0]==search_id:
+        print("student Found:",line)
+        found=True
+file.close()
+'''
+
+#Step 3: Delete Operation
+delete_id = "101"
+file=open("students_details.txt","r")
+lines=file.readlines()
+file.close()
+
+file=open("students_details.txt","w")
+for line in lines:
+    data=line.strip().split(",")
+    if data[0]!=delete_id:
+        file.write(line)
+file.close()
+    
