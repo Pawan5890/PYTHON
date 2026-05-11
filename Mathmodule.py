d25c1a6ec7d59b7f71896e0e39b74d643184dab7
Pawan kumar <kumarpy5890@gmail.com>
2026-05-11 13:19:29 +0530

    Create Mathmodule.py

---
+++ Mathmodule.py
@@ -0,0 +1,30 @@
+#Math module :- It is built in python library for mathematical operations .
+#important function includes trignometry , logarithm , constants, power operations
+#Important constants:- math pi -> π (3.142) and math e -> Euler number (2.71)
+#Trignometric functons :- math.sin(x) -> sin e , math.cos(x) ->cosine , math.tan(x) ->tangent
+
+#Degree <--> Radian conucrsion
+#Radian conversion of degree:- math.radians(angle)          π radian ->180
+#Degree conversion of radian:- math.degree(value)           180->π radian , 1->π/180 radians
+
+#power and roots :-
+#math.pow(x,y) ->x^y ; Area of circle =  πr^2
+#math.sqrt(x) ->square root    =(math.pi).(math.pow(r,2))
+
+#Logarithmic Functions:-
+#math.log(x) ->natural log -> lnx -> logex
+# math.log 10(x) -> log10X
+
+#Rounding Functions :-
+#math.ceil(x) -> round up -> Eg: math.ceil(5.45) -> 6
+#math.floor(x) -> round down -> Eg: math.floor(5.45) -> 5
+
+#Factorial :-
+#math.factorial(n) -> n!
+
+#Round() :- It is used to round numbers
+#syntax :- round(number,digits)
+#number-> value to round
+#Digit -> number of decimal places
+#x= round(4.6), print(x) output->5
+#x=round(3,14159,2)->3.14
